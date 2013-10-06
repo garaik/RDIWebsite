ad861e9e4614f7d03900532c4dc02c2a4c040ac5
Krisztian Garai <krisztian.garai@gmail.com>
2013-10-06 13:02:21 +0200

    Added content in another style.

--- javascripts/rdi/rdi.js
+++ javascripts/rdi/rdi.js
@@ -1,57 +1,79 @@
 $(function() {
-    var dur = 400;
-    var st = $('#slogen-title-1');
-    var sd = $('#slogen-description-1');
-    var defaultCss = {
-        st: {
-            left: st.css('left'),
-            top: st.css('top'),
-            opacity: 0,
-            display: "block"
-        },
-        sd: {
-            left: sd.css('left'),
-            top: sd.css('top'),
-            opacity: 0,
-            display: "block"
-        }
+    var startSlogenAnim = function() {
+        var dur = 400;
+        var st = $('#slogen-title-1');
+        var sd = $('#slogen-description-1');
+        var defaultCss = {
+            st: {
+                left: st.css('left'),
+                top: st.css('top'),
+                opacity: 0,
+                display: "block"
+            },
+            sd: {
+                left: sd.css('left'),
+                top: sd.css('top'),
+                opacity: 0,
+                display: "block"
+            }
+        };
+        var i = 1;
+        window.setInterval(function() {
+            st = $('#slogen-title-' + i);
+            sd = $('#slogen-description-' + i);
+            st.animate({
+                top: 0,
+                opacity: 0
+            }, {
+                duration: dur,
+                ease: 'ease-in',
+                complete: function() {
+                    $(this).css({ display: "none"})
+                }
+            });
+            sd.animate({
+                left: 200,
+                opacity: 0
+            }, {
+                duration: dur,
+                ease: 'ease-in',
+                complete: function() {
+                    $(this).css({ display: "none"})
+                }
+            });
+            if (++i == 4) {
+               i = 1;
+            }
+            st = $('#slogen-title-' + i);
+            sd = $('#slogen-description-' + i);
+            st.css(defaultCss.st);
+            sd.css(defaultCss.sd);
+            st.animate({
+                opacity: 1
+            }, dur, 'ease-in');
+            sd.animate({
+                opacity: 1
+            }, dur, 'ease-in');
+        }, 3000);
     };
-    var i = 1;
-    window.setInterval(function() {
-        st = $('#slogen-title-' + i);
-        sd = $('#slogen-description-' + i);
-        st.animate({
-            top: 0,
-            opacity: 0
-        }, {
-            duration: dur,
-            ease: 'ease-in',
-            complete: function() {
-                $(this).css({ display: "none"})
-            }
+
+    var startPizzaInteraction = function() {
+        var pizzaSvg = document.getElementById('pizzasvg');
+        pizzaSvg.addEventListener("load", function() {
+            var svgDoc = pizzaSvg.contentDocument;
+            var svgRoot = svgDoc.documentElement;
+            $('#slice-research', svgRoot).click(function() {
+                $(this).remove();
+            });
+            $('#slice-development', svgRoot).click(function() {
+                $(this).remove();
+            });
+            $('#slice-innovation', svgRoot).click(function() {
+                $(this).remove();
+            });
         });
-        sd.animate({
-            left: 200,
-            opacity: 0
-        }, {
-            duration: dur,
-            ease: 'ease-in',
-            complete: function() {
-                $(this).css({ display: "none"})
-            }
-        });
-        if (++i == 4) {
-           i = 1;
-        }
-        st = $('#slogen-title-' + i);
-        sd = $('#slogen-description-' + i);
-        st.css(defaultCss.st);
-        sd.css(defaultCss.sd);
-        st.animate({
-            opacity: 1
-        }, dur, 'ease-in');
-        sd.animate({
-            opacity: 1
-        }, dur, 'ease-in');
-    }, 3000);
+    };
+
+//    startSlogenAnim();
+    startPizzaInteraction();
 });
