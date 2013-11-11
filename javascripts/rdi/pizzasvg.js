b5be5d95eee33bcb026d3cd70b02526eec084d02
garaik <b3b1c5k3>
2013-11-11 18:11:41 +0100

    Get JS work.

--- javascripts/rdi/pizzasvg.js
+++ javascripts/rdi/pizzasvg.js
@@ -1,69 +1,86 @@
-var PizzaSVG = (function() {
+var PIZZA_SVG = (function() {
+    var angle = 0;
+    var animate = false;
+    var callback;
+
+    var x = [];
+    x[0] = { R: 0, D: -120, I: 120 };
+    x[120] = { R: -120, D: 120, I: 0 };
+    x[240] = { R: 120, D: 0, I: -120 };
+
+    var rotate = function(slice) {
+        if (!animate) {
+            animate = true;
+            var angleMod = (angle % 360 + 360) % 360;
+            var t = x[angleMod][slice];
+            var d;
+            if (t == 0) {
+                d = 30;
+                $('#slices, #outlines, #captions')
+                        .animate({
+                            svgTransform: 'rotate(' + (angle - 3) + ', 140, 140)'
+                        }, d)
+                        .animate({
+                            svgTransform: 'rotate(' + (angle + 3) + ', 140, 140)'
+                        }, d * 2)
+                        .animate({
+                            svgTransform: 'rotate(' + (angle - 3) + ', 140, 140)'
+                        }, d * 2)
+                        .animate({
+                            svgTransform: 'rotate(' + (angle + 3) + ', 140, 140)'
+                        }, d * 2)
+                        .animate({
+                            svgTransform: 'rotate(' + angle + ', 140, 140)'
+                        }, d, function() { animate = false; });
+            }
+            else {
+                callback && callback(slice);
+                angle += t;
+                d = 400;
+                $('#slices').animate({
+                    svgTransform: 'rotate(' + angle + ', 140, 140)'
+                }, d);
+                $('#outlines').delay(40).animate({
+                    svgTransform: 'rotate(' + angle + ', 140, 140)'
+                }, d);
+                $('#captions').delay(80).animate({
+                    svgTransform: 'rotate(' + angle + ', 140, 140)'
+                }, d, function () {
+                    animate = false;
+                });
+            }
+        }
+    };
+
+    $(function() {
+        $('#pizzasvg').svg({
+            loadURL: 'images/rdipizza.svg',
+            onLoad: function (svg) {
+                $('#slices, #captions, #outlines').attr('transform', 'rotate(0, 140, 140)');
+                var di = 'data-index';
+                $('#slice-research').attr(di, 'R');
+                $('#slice-development').attr(di, 'D');
+                $('#slice-innovation').attr(di, 'I');
+                $('#slice-research, #slice-development, #slice-innovation').click(function () {
+                    var slice = $(this).attr(di);
+                    rotate(slice);
+                });
+            }
+        });
+    });
 
     return {
-        switchResearch: function() {},
-        switchInnovation: function() {},
-        switchDevelopment: function() {}
+        switchResearch: function() {
+            rotate('R');
+        },
+        switchDevelopment: function() {
+            rotate('D')
+        },
+        switchInnovation: function() {
+            rotate('I');
+        },
+        setCallback: function(cb) {
+            callback = cb;
+        }
     }
 })();
-
-$(function () {
-    $('#pizzasvg').svg({
-        loadURL: 'images/rdipizza.svg',
-        onLoad: function (svg) {
-            $('#slices, #captions, #outlines').attr('transform', 'rotate(0, 140, 140)');
-            var angle = 0;
-            var di = 'data-index';
-            $('#slice-research').attr(di, 'R');
-            $('#slice-development').attr(di, 'D');
-            $('#slice-innovation').attr(di, 'I');
-            var x = [];
-            x[0] = { R: 0, D: -120, I: 120 };
-            x[120] = { R: -120, D: 120, I: 0 };
-            x[240] = { R: 120, D: 0, I: -120 };
-            var animate = false;
-            $('#slice-research, #slice-development, #slice-innovation').click(function () {
-                if (!animate) {
-                    animate = true;
-                    var angleMod = (angle % 360 + 360) % 360;
-                    var t = x[angleMod][$(this).attr(di)];
-                    var d;
-                    if (t == 0) {
-                        d = 30;
-                        $('#slices, #outlines, #captions')
-                                .animate({
-                                    svgTransform: 'rotate(' + (angle - 3) + ', 140, 140)'
-                                }, d)
-                                .animate({
-                                    svgTransform: 'rotate(' + (angle + 3) + ', 140, 140)'
-                                }, d * 2)
-                                .animate({
-                                    svgTransform: 'rotate(' + (angle - 3) + ', 140, 140)'
-                                }, d * 2)
-                                .animate({
-                                    svgTransform: 'rotate(' + (angle + 3) + ', 140, 140)'
-                                }, d * 2)
-                                .animate({
-                                    svgTransform: 'rotate(' + angle + ', 140, 140)'
-                                }, d, function() { animate = false; });
-                    }
-                    else {
-                        angle += t;
-                        d = 400;
-                        $('#slices').animate({
-                            svgTransform: 'rotate(' + angle + ', 140, 140)'
-                        }, d);
-                        $('#outlines').delay(40).animate({
-                            svgTransform: 'rotate(' + angle + ', 140, 140)'
-                        }, d);
-                        $('#captions').delay(80).animate({
-                            svgTransform: 'rotate(' + angle + ', 140, 140)'
-                        }, d, function () {
-                            animate = false;
-                        });
-                    }
-                }
-            });
-        }
-    });
-});
--- javascripts/rdi/rdi.js
+++ javascripts/rdi/rdi.js
@@ -1,99 +1,95 @@
-$(function() {
-    var startSlogenAnim = function() {
-        var dur = 400;
-        var st = $('#slogen-title-1');
-        var sd = $('#slogen-description-1');
-        var defaultCss = {
-            st: {
-                left: st.css('left'),
-                top: st.css('top'),
-                opacity: 0,
-                display: "block"
+RDI_CONTENT = (function() {
+    var rdi;
+    var current = 'r';
+    var delay = 400;
+
+    function setHeightAndInit(x, p) {
+        x.css({
+            display: 'block',
+            position: 'fixed',
+            left: -8000,
+            width: p.width()
+        });
+        x.attr('data-height', x.height());
+        x.css({
+            height: 0,
+            opacity: 0,
+            position: 'static',
+            left: 0,
+            overflow: 'hidden'
+        });
+    }
+
+    $(function() {
+        var g = $('.gear');
+        var q = g.find('.quotation');
+        var d = g.find('.division-content');
+        var a = g.find('.division-aside');
+        rdi = {
+            r: {
+                q: q.find('.research'),
+                d: d.find('.research'),
+                a: a.find('.research')
             },
-            sd: {
-                left: sd.css('left'),
-                top: sd.css('top'),
-                opacity: 0,
-                display: "block"
+            d: {
+                q: q.find('.development'),
+                d: d.find('.development'),
+                a: a.find('.development')
+            },
+            i: {
+                q: q.find('.innovation'),
+                d: d.find('.innovation')
             }
         };
-        var i = 1;
-        window.setInterval(function() {
-            st = $('#slogen-title-' + i);
-            sd = $('#slogen-description-' + i);
-            st.animate({
-                top: 0,
-                opacity: 0
-            }, {
-                duration: dur,
-                ease: 'ease-in',
-                complete: function() {
-                    $(this).css({ display: "none"})
-                }
-            });
-            sd.animate({
-                left: 200,
-                opacity: 0
-            }, {
-                duration: dur,
-                ease: 'ease-in',
-                complete: function() {
-                    $(this).css({ display: "none"})
-                }
-            });
-            if (++i == 4) {
-               i = 1;
-            }
-            st = $('#slogen-title-' + i);
-            sd = $('#slogen-description-' + i);
-            st.css(defaultCss.st);
-            sd.css(defaultCss.sd);
-            st.animate({
-                opacity: 1
-            }, dur, 'ease-in');
-            sd.animate({
-                opacity: 1
-            }, dur, 'ease-in');
-        }, 3000);
-    };
 
-    var initTopMenu = function() {
-        var pos;
-        var current = 144;
-        var target;
-        var $w = $(window);
-        var mb = $('#menubar');
-        var mbimg = mb.find('.logo img');
-        $w.scroll(function() {
-            pos = $w.scrollTop();
-            target = pos > 84 ? 60 : 144 - pos;
+        [rdi.r.d, rdi.d.d, rdi.i.d].forEach(function(x) {
+            setHeightAndInit(x, d);
         });
-        window.setInterval(function() {
-            if (current < target) {
-                current += 5;
-                if (current > target) {
-                    current = target;
-                }
-            }
-            else if (current > target) {
-                current -= 5;
-                if (current < target) {
-                    current = target;
-                }
-            }
-            mb.css({ height: current });
-            mbimg.css({ maxHeight: current });
-        }, 5);
-    };
 
-    var initProducts = function() {
-        $('.products').css({
-            display: 'none'
-        });
-    };
+        setHeightAndInit(rdi.r.a, a);
+        setHeightAndInit(rdi.d.a, a);
 
-    var initReferences = function() {
-        var refs = [];
+        show(current);
+    });
+
+    function show(w) {
+        var rdiw = rdi[w];
+        rdiw.q.css({
+            display: 'block'
+        }, delay);
+        rdiw.d.animate({
+            height: rdiw.d.attr('data-height'),
+            opacity: 1
+        }, delay);
+        if (rdiw.a) {
+            rdiw.a.animate({
+                height: rdiw.a.attr('data-height'),
+                opacity: 1
+            }, delay);
+        }
+        current = w;
+    }
+
+    function hide() {
+        var rdic = rdi[current];
+        rdic.q.css({ display: 'none' }, delay);
+        rdic.d.animate({ height: 0, opacity: 0 }, delay);
+        if (rdic.a) {
+            rdic.a.animate({ height: 0, opacity: 0 }, delay);
+        }
+    }
+
+    return {
+        switchResearch: function() { hide(); show('r'); },
+        switchDevelopment: function() { hide(); show('d'); },
+        switchInnovation: function() { hide(); show('i'); }
+    }
+})();
+
+REFERENCES = (function() {
+    var refs = [];
+
+    $(function() {
         var $refItemsUl = $('#reference-items').find('>ul:first-child');
         $refItemsUl.find('li').each(function() {
             var $this = $(this);
@@ -160,106 +156,111 @@ $(function() {
             }
             return false;
         });
-    };
+    });
+})();
 
-    var RDIContent = (function() {
-        var g = $('.gear');
-        var q = g.find('.quotation');
-        var d = g.find('.division-content');
-        var a = g.find('.division-aside');
-        var rdi = {
-            r: {
-                q: q.find('.research'),
-                d: d.find('.research'),
-                a: a.find('.research')
-            },
-            d: {
-                q: q.find('.development'),
-                d: d.find('.development'),
-                a: a.find('.development')
-            },
-            i: {
-                q: q.find('.innovation'),
-                d: d.find('.innovation')
-            }
+SLOGEN = (function() {
+    var dur = 500;
+    var defaultCss;
+    var i = 1;
+
+    $(function() {
+        var st = $('#slogen-title-1');
+        var sd = $('#slogen-description-1');
+        defaultCss = {
+           st: {
+               left: st.css('left'),
+               top: st.css('top'),
+               opacity: 0,
+               display: "block"
+           },
+           sd: {
+               left: sd.css('left'),
+               top: sd.css('top'),
+               opacity: 0,
+               display: "block"
+           }
         };
 
-        function setHeightAndInit(x, p) {
-            x.css({
-                display: 'block',
-                position: 'fixed',
-                left: -8000,
-                width: p.width()
+        window.setInterval(function() {
+            $('#slogen-title-' + i).animate({
+                top: 0,
+                opacity: 0
+            }, {
+                duration: dur,
+                complete: function() {
+                    $(this).css({ display: "none"})
+                }
             });
-            x.attr('data-height', x.height());
-            x.css({
-                height: 0,
-                opacity: 0,
-                position: 'static',
-                left: 0,
-                overflow: 'hidden'
+            $('#slogen-description-' + i).animate({
+                left: 200,
+                opacity: 0
+            }, {
+                duration: dur,
+                complete: function() {
+                    $(this).css({ display: "none"})
+                }
             });
-        }
+            if (++i == 4) { i = 1; }
+            $('#slogen-title-' + i).css(defaultCss.st).animate({ opacity: 1 }, dur);
+            $('#slogen-description-' + i).css(defaultCss.sd).animate({ opacity: 1 }, dur);
+        }, 7000);
+    });
+})();
 
-        var current = 'r';
-        var delay = 400;
+TOP_MENU = (function(w) {
+    var pos;
+    var current = 144;
+    var target = 144;
 
-        function show(w) {
-            var rdiw = rdi[w];
-            rdiw.q.css({
-                display: 'block'
-            }, delay);
-            rdiw.d.animate({
-                height: rdiw.d.attr('data-height'),
-                opacity: 1
-            }, delay);
-            if (rdiw.a) {
-                rdiw.a.animate({
-                    height: rdiw.a.attr('data-height'),
-                    opacity: 1
-                }, delay);
-            }
-            current = w;
-        }
-
-        function hide() {
-            var rdic = rdi[current];
-            rdic.q.css({ display: 'none' }, delay);
-            rdic.d.animate({ height: 0, opacity: 0 }, delay);
-            if (rdic.a) {
-                rdic.a.animate({ height: 0, opacity: 0 }, delay);
-            }
-        }
-
-        [rdi.r.d, rdi.d.d, rdi.i.d].forEach(function(x) {
-            setHeightAndInit(x, d);
+    $(function() {
+        $(window).scroll(function() {
+            pos = $(this).scrollTop();
+            target = pos > 84 ? 60 : 144 - pos;
         });
-        setHeightAndInit(rdi.r.a, a);
-        setHeightAndInit(rdi.d.a, a);
 
-        show(current);
+        window.setInterval(function() {
+            if (current < target) {
+                current += 10;
+                if (current > target) {
+                    current = target;
+                }
+            }
+            else if (current > target) {
+                current -= 10;
+                if (current < target) {
+                    current = target;
+                }
+            }
+            $('#menubar').css({ height: current });
+            $('#menu-logo').css({ maxHeight: current });
+        }, 20);
+    });
+})();
 
-        return {
-            switchResearch: function() { hide(); show('r'); },
-            switchDevelopment: function() { hide(); show('d'); },
-            switchInnovation: function() { hide(); show('i'); }
-        }
-    })();
-
-//    startSlogenAnim();
-//    initTopMenu();
-    initProducts();
-    initReferences();
+$(function() {
+    $('.products').css({ display: 'none' });
 
     $('#menu-research').click(function() {
-        RDIContent.switchResearch(); return false;
+        PIZZA_SVG.switchResearch();
+        return false;
     });
     $('#menu-development').click(function() {
-        RDIContent.switchDevelopment(); return false;
+        PIZZA_SVG.switchDevelopment();
+        return false;
     });
     $('#menu-innovation').click(function() {
-        RDIContent.switchInnovation(); return false;
+        PIZZA_SVG.switchInnovation();
+        return false;
+    });
+    PIZZA_SVG.setCallback(function(slice) {
+        switch (slice) {
+            case 'R':
+                RDI_CONTENT.switchResearch(); break;
+            case 'D':
+                RDI_CONTENT.switchDevelopment(); break;
+            case 'I':
+                RDI_CONTENT.switchInnovation(); break;
+        }
     });
 });
-
-// http://www.1stwebdesigner.com/wp-content/uploads/2010/06/nagging-menu-with-css3-and-jquery/index.html
